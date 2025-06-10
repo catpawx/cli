9cfc380b17050cfd11ea6d252e34156ae45517e4
相甫 <xiangfu_git@163.com>
2025-06-10 20:27:12 +0800

    build: 修改GitHub工作流

--- .releaserc.js
+++ .releaserc.js
@@ -15,6 +15,7 @@ module.exports = {
       '@semantic-release/git',
       {
         assets: ['CHANGELOG.md', 'package.json'], // 前面说到日志记录和版本好是新增修改的，需要 push 回 Git
+        message: 'chore: ${nextRelease.version} [skip ci]',
       },
     ],
   ],
